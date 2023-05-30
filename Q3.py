d2ff9a3ae59458a85b8e8a0d5f5fb951cc98a417
guynimri <guynimri@campus.technion.ac.il>
2023-05-30 16:13:19 +0300

    add image

--- Q3.py
+++ Q3.py
@@ -39,7 +39,8 @@ class cyclic_group_order_4():
 def Q_3_a():
     # use the cyclic group and apply a rotation on an image
     # load image
-    img = cv2.imread("photo-1592066162862-aad179660a54.png")
-    img.imshow()
+    img = cv2.imread("Lena-128-128-Original-Lena-image-a-and-its-reconstructions-using-respectively-b.png")
+    cv2.imshow("original image", img)
+    cv2.waitKey(0)
 
 Q_3_a()
