a83c0c58a7e43d0440ebcef3f247f5dc0d8758ab
GalYona21 <63791604+GalYona21@users.noreply.github.com>
2023-05-30 13:48:59 +0300

    Add files via upload

---
+++ Q4_b.py
@@ -0,0 +1,124 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+# create ring graph with n nodes
+def create_graph_ring_laplacian(n):
+    adj = np.zeros((n, n))
+    for i in range(n):
+        adj[i, (i + 1) % n] = 1
+        adj[(i + 1) % n, i] = 1
+    D = np.diag(np.sum(adj, axis=1))
+    L = D - adj
+    return L
+
+
+
+# create the cartesian product of two graphs
+def cartesian_product_graph_laplacian(L1, L2):
+    n1 = L1.shape[0]
+    n2 = L2.shape[0]
+    L = np.zeros((n1 * n2, n1 * n2))
+    adj = np.zeros((n1 * n2, n1 * n2))
+    for i in range(n1):
+        for j in range(n2):
+            for k in range(n1):
+                for l in range(n2):
+                    if L1[i, k] == -1 or L2[j, l] == -1:
+                        adj[i * n2 + j, k * n2 + l] = 1
+                        adj[k * n2 + l, i * n2 + j] = 1
+    D = np.diag(np.sum(adj, axis=1))
+    L = D - adj
+    return L
+
+def compute_eigenvectors(L, k):
+    eig, U = np.linalg.eig(L)
+    eig = np.real(eig)
+    idx = eig.argsort()
+
+    eig = eig[idx]
+    U = U[:, idx]
+    return eig, U[:, :k]
+
+def plot_graph_topology(eigvecs=None):
+    # Create the first ring graph with n nodes
+    n = 20
+    ring_graph_1 = nx.cycle_graph(n)
+
+    # Create the second ring graph with m nodes
+    m = 50
+    ring_graph_2 = nx.cycle_graph(m)
+    cartesian_product = nx.cartesian_product(ring_graph_1, ring_graph_2)
+
+    pos = nx.spring_layout(cartesian_product)
+    fig, ax = plt.subplots()
+    if eigvecs is None:
+        nx.draw(cartesian_product, ax=ax,pos=pos, node_size=10, width=0.5)
+    else:
+        nx.draw(cartesian_product, ax=ax, pos=pos, node_size=10, width=0.5, node_color=eigvecs[:, 0])
+        ax.set_title("Cartesian Product of Two Ring Graphs Colored by First Eigenvector")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        nx.draw(cartesian_product, ax=ax, pos=pos, node_size=10, width=0.5, node_color=eigvecs[:, 1])
+        ax.set_title("Cartesian Product of Two Ring Graphs Colored by Second Eigenvector")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        nx.draw(cartesian_product, ax=ax, pos=pos, node_size=10, width=0.5, node_color=eigvecs[:, 5])
+        ax.set_title("Cartesian Product of Two Ring Graphs Colored by Sixth Eigenvector")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        nx.draw(cartesian_product, ax=ax, pos=pos, node_size=10, width=0.5, node_color=eigvecs[:, 9])
+        ax.set_title("Cartesian Product of Two Ring Graphs Colored by Tenth Eigenvector")
+        plt.show()
+
+    ax.set_title("Cartesian Product of Two Ring Graphs")
+    plt.show()
+
+def plot_eig_values(eigs):
+    fig, ax = plt.subplots()
+    ax.plot(eigs, 'o')
+    ax.set_title("Eigenvalues of the Cartesian Product Graph")
+    plt.show()
+
+
+
+ring_20_laplacian = create_graph_ring_laplacian(20)
+ring_50_laplacian = create_graph_ring_laplacian(50)
+L = cartesian_product_graph_laplacian(ring_20_laplacian, ring_50_laplacian)
+print(L)
+
+# 1
+eigs, eig_vecs = compute_eigenvectors(L, 10)
+
+
+# 2
+# a
+plot_graph_topology()
+
+# b
+plot_eig_values(eigs)
+
+# c
+plot_graph_topology(eig_vecs)
+
+# G = nx.Graph()
+# G.add_nodes_from(range(20*50))
+# # add edges to the cartesian product of G1 and G2
+# for i in range(20):
+#     for j in range(50):
+#         for k in range(20):
+#             for l in range(50):
+#                 if ring_20_laplacian[i, k] == -1 or ring_50_laplacian[j, l] == -1:
+#                     G.add_edge(i * 50 + j, k * 50 + l)
+#
+#
+#
+# # Plot graph topology
+# fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+# nx.draw(G, ax=ax[1], pos=nx.Graph(G), node_size=10, width=0.1)
+# ax[0].set_title("Cartesian Product Graph G1 x G2")
+# plt.show()
+#
---
+++ Q5.py
@@ -0,0 +1,158 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import open3d as o3d
+import pyvista as pv
+
+
+def load_3d_point_cloud():
+    # Load the XYZ coordinates from the OFF file
+    with open('tent_0163.off', 'r') as f:
+        lines = f.readlines()
+
+    points = []
+    for line in lines[1:]:
+        if len(line.split()) == 3:
+            x, y, z = line.split()
+        points.append([float(x), float(y), float(z)])
+
+    # Convert the points list to a numpy array
+    points = np.array(points)
+    avg = np.mean(points, axis=0)
+    return points, avg
+
+
+def plot_via_matplotlib(points):
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the point cloud
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='b', marker='o')
+
+    # Set axis labels
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # Show the plot
+    plt.show()
+
+
+def plot_via_open3d(points):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    o3d.visualization.draw_geometries([pcd])
+
+def plot_via_pyvista(points):
+    cloud = pv.PolyData(points)
+    cloud.plot()
+
+# add noise sampled from N(0,0.01s^2I) to the point cloud
+def add_noise(points, s=0.01):
+    noise = np.random.normal(0, (0.01 * (s**2)) , points.shape)
+    return points + noise
+
+
+# Construct a sparse affinity matrix by a Gaussian kernel with the Euclidean distance in a
+def construct_affinity_matrix(points, r=100):
+    # Compute the pairwise Euclidean distances
+    distances = np.sqrt(((points[:, None] - points) ** 2).sum(axis=2))
+
+    # Compute the affinity matrix by a Gaussian kernel
+    W = np.exp(-distances ** 2 / (2 * r ** 2))
+    return W
+
+# Compute the eigendecomposition of the normalized Laplacian matrix and plot the sorted eigenvalues from the smallest to the largest.
+def calculate_normalized_laplacian(W):
+    # Compute the degree vector
+    d = W.sum(axis=1)
+
+    # Compute the Laplacian matrix
+    D = np.diag(d)
+    L = D - W
+
+    # Compute the normalized Laplacian matrix
+    d_sqrt_inv = np.sqrt(np.linalg.inv(D))
+    L_norm = d_sqrt_inv.dot(L).dot(d_sqrt_inv)
+    return L_norm
+
+
+
+# Plot the noisy 3D point cloud colored by first, second, sixth, and tenth eigenvectors.
+def plot_3d_point_cloud_with_colors(points, normalized_laplacian, plot=True):
+    eig, U = np.linalg.eig(normalized_laplacian)
+    eig = np.real(eig)
+    idx = eig.argsort()
+
+    eig = eig[idx]
+    U = U[:, idx]
+
+    if plot:
+        # first eigenvector colorization
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=U[:, 0], marker='o')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+
+        # second eigenvector colorization
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=U[:, 1], marker='o')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+
+        # sixth eigenvector colorization
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=U[:, 5], marker='o')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+
+        # tenth eigenvector colorization
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=U[:, 9], marker='o')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+
+
+    return eig
+
+def low_pass_filter(points, lambda_max, tau=0.1):
+    # apply h(x)=exp(-tau*x/lambda_max)
+    h = lambda x: np.exp(-tau * x / lambda_max)
+
+    denoised_points = h(points)
+    return denoised_points
+
+
+
+points, avg = load_3d_point_cloud()
+# a
+plot_via_pyvista(points)
+# b
+noisy_points = add_noise(points, s=np.mean(avg))
+plot_via_pyvista(noisy_points)
+# plot_via_matplotlib(noisy_points)
+# c
+W = construct_affinity_matrix(noisy_points, r=np.mean(avg))
+L_norm = calculate_normalized_laplacian(W)
+# d
+eig = plot_3d_point_cloud_with_colors(noisy_points, L_norm,plot=False)
+# e
+denoised_points = low_pass_filter(noisy_points, eig[0])
+plot_via_pyvista(denoised_points)
+# plot_via_open3d(denoised_points)
+
+
+
+
